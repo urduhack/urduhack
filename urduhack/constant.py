5745b1ddde9489298c732d7249faae961fa40c85
Ikram Ali <mrikram1989@gmail.com>
2018-12-30 11:37:12 +0500

    [normalization] char level mapping.

---
+++ urduhack/constant.py
@@ -0,0 +1,6 @@
+# coding: utf8
+"""Constant module"""
+
+import regex as re
+
+ONLY_URDU_RE = re.compile(r"[]")
---
+++ urduhack/normalization/character/tests/test_mapping.py
@@ -0,0 +1,9 @@
+# coding: utf8
+"""Test cases for mapping"""
+
+from urduhack.normalization.character.mapping import normalize_characters
+
+
+def test_normalize_characters():
+    """Test case"""
+    assert True
