8a93fd5ed22da1064f33fd37f2aaef45881e0e79
Ikram Ali <mrikram1989@gmail.com>
2019-01-06 14:03:34 +0500

    [space] words level space fixes.

--- urduhack/normalization/space/util.py
+++ urduhack/normalization/space/util.py
@@ -1,13 +1,16 @@
 # coding: utf8
 """Space utils"""
 import regex as re
+from urduhack.urdu_characters import URDU_ALL_CHARACTERS, URDU_PUNCTUATIONS
 
 # Add spaces before|after numeric number and urdu words
 # 18سالہ  , 20فیصد
-SPACE_BEFORE_DIGITS_RE = re.compile(r"(?<=[\u0600-\u06ff])(?=[0-9])", flags=re.U | re.M | re.I)
-SPACE_AFTER_DIGITS_RE = re.compile(r"(?<=[0-9])(?=[\u0600-\u06ff])", flags=re.U | re.M | re.I)
+SPACE_BEFORE_DIGITS_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[0-9])", flags=re.U | re.M | re.I)
+SPACE_AFTER_DIGITS_RE = re.compile(r"(?<=[0-9])(?=[" + "".join(URDU_ALL_CHARACTERS) + "])", flags=re.U | re.M | re.I)
 # Add spaces after ., if there is number then not Ex (9.00)
-SPACE_AFTER_PUNCTUATIONS_RE = re.compile(r"(?<=[۔،])(?=[^\s0-9۔،])", flags=re.U | re.M | re.I)
+SPACE_AFTER_PUNCTUATIONS_RE = re.compile(
+        r"(?<=[" + "".join(URDU_PUNCTUATIONS) + "])(?=[^" + "".join(URDU_PUNCTUATIONS) + "0-9 ])",
+        flags=re.U | re.M | re.I)
 
 
 def digits_space(text: str) -> str:
