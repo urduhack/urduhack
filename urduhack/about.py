38f86f5be7cc3c30923a091e2aed2e352e7b40de
Ikram Ali <mrikram1989@gmail.com>
2019-01-11 14:10:24 +0500

    [docs] documentation added for different tasks.

--- urduhack/about.py
+++ urduhack/about.py
@@ -2,4 +2,8 @@
 """Project Meta data description"""
 
 __version__ = '0.0.1'
-DESCRIPTION = 'Natural Language Processing (NLP) library for Urdu language.'
+__description__ = 'Natural Language Processing (NLP) library for Urdu language.'
+__url__ = 'https://urduhack.readthedocs.io'
+__author__ = 'Ikram Ali'
+__author_email__ = 'mrikram1989@gmail.com'
+__license__ = 'MIT License'
