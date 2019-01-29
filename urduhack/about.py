22c18d4b17830e8ea464682600ae19cea0bb254d
Ikram Ali <mrikram1989@gmail.com>
2019-01-29 15:05:47 +0500

    [release] version 0.0.2 released.

--- urduhack/about.py
+++ urduhack/about.py
@@ -1,7 +1,7 @@
 # coding: utf8
 """Project Meta data description"""
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __description__ = 'Natural Language Processing (NLP) library for Urdu language.'
 __url__ = 'https://urduhack.readthedocs.io'
 __author__ = 'Ikram Ali'
