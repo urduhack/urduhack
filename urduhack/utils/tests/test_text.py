792d16e63a47dc6454250c7195bfae64b931b42c
mujadadrao@gmail.com <A1b2c3d4f5g6>
2019-01-29 12:51:18 +0500

    [diacritics+docs] test_diacritics func fixed, more test cases added, documentation for utils.text module added!

--- urduhack/utils/tests/test_text.py
+++ urduhack/utils/tests/test_text.py
@@ -1,20 +1,20 @@
 # coding: utf8
 """Test cases for text.py file"""
 
-from urduhack.urdu_characters import URDU_DIACRITICS
+from urduhack.urdu_characters import URDU_DIACRITICS, URDU_ALPHABETS
 from ..text import remove_diacritics
 
 
 def test_remove_diacritics():
     """remove_diacritics Test case"""
     words: dict = {"اب": "اَب",
-                   "شیر پنجاب‬": "شیرِ پنجاب‬",
-                   "او‬گول‬": "اُو‬گول‬",
+                   "شیر پنجاب": "شیرِ پنجاب",
+                   "اوگول": "اُوگول",
                    "ای": "اِی",
-                   "اباو‬گل": "اَباُو‬گل",
+                   "اباوگل": "اَباُوگل",
                    "شرپن": "شرِپن",
-                   "اا‬ایول": "اَاُ‬اِیول",
-                   "اے‬": "اَے‬",
+                   "ااایول": "اَاُاِیول",
+                   "اے": "اَے",
                    "اوشیر": "اُوشیر",
                    "او": "اَو",
                    }
@@ -24,3 +24,6 @@ def test_remove_diacritics():
         assert key == norm
         for char in norm:
             assert char not in URDU_DIACRITICS, norm
+
+            if char != ' ':
+                assert char in URDU_ALPHABETS, norm
