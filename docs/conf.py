8c282af7a7fff54113f41ac54068deae8be9e607
Ikram Ali <mrikram1989@gmail.com>
2019-02-15 22:59:24 +0500

    [docs] sphinx napoleon ext added.

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
