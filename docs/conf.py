1fec75bc957952ff634fb2c375716744260adbc5
Ikram Ali <mrikram1989@gmail.com>
2019-02-19 14:30:48 +0500

    [docs] custom urduhack.css file added.

--- docs/conf.py
+++ docs/conf.py
@@ -94,6 +94,12 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/urduhack.css'
+    ]
+}
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
