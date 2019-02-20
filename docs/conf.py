028deaebdcac7d0c53bd9696840772baa8968143
Ikram Ali <mrikram1989@gmail.com>
2019-02-20 11:04:33 +0500

    [docs] logo added.

--- docs/conf.py
+++ docs/conf.py
@@ -87,8 +87,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
-
+html_theme_options = {
+    'logo_only': True,
+}
+html_logo = '_static/urduhack.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
