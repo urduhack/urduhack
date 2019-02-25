a1d42968e81183e166060e4bbca63a0bf2c02653
Ikram Ali <mrikram1989@gmail.com>
2019-02-25 12:06:48 +0500

    Contribs (#19)
    [normalization] normalization removed fix_join_words

--- urduhack/normalization/util.py
+++ urduhack/normalization/util.py
@@ -6,21 +6,17 @@ Normalization Util module
 The util module provides functionality to put proper spaces before and after numeric digits, urdu digits
 and punctuations.
 """
-from typing import Dict
 
 import regex as re
 
-from urduhack.urdu_characters import URDU_ALL_CHARACTERS, URDU_PUNCTUATIONS, \
-    URDU_DIACRITICS
+from urduhack.urdu_characters import URDU_ALL_CHARACTERS, URDU_PUNCTUATIONS, URDU_DIACRITICS
 
 # Add spaces before|after numeric number and urdu words
 # 18سالہ  , 20فیصد
 EXCEPT_HAMZA = list(filter(lambda c: c != '\u0621', URDU_ALL_CHARACTERS))
-SPACE_BEFORE_DIGITS_RE = re.compile(
-    r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[0-9])",
-    flags=re.U | re.M | re.I)
-SPACE_AFTER_DIGITS_RE = re.compile(
-    r"(?<=[0-9])(?=[" + "".join(EXCEPT_HAMZA) + "])", flags=re.U | re.M | re.I)
+SPACE_BEFORE_DIGITS_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[0-9])",
+                                    flags=re.U | re.M | re.I)
+SPACE_AFTER_DIGITS_RE = re.compile(r"(?<=[0-9])(?=[" + "".join(EXCEPT_HAMZA) + "])", flags=re.U | re.M | re.I)
 
 
 def digits_space(text: str) -> str:
@@ -41,11 +37,10 @@ def digits_space(text: str) -> str:
 
 # Add spaces after ., if there is number then not Ex (9.00)
 SPACE_AFTER_PUNCTUATIONS_RE = re.compile(
-    r"(?<=[" + "".join(URDU_PUNCTUATIONS) + "])(?=[^" + "".join(
-        URDU_PUNCTUATIONS) + "0-9 \n])",
-    flags=re.U | re.M | re.I)
-REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE = re.compile(
-    r'\s+([' + "".join(URDU_PUNCTUATIONS) + '])', flags=re.U | re.M | re.I)
+        r"(?<=[" + "".join(URDU_PUNCTUATIONS) + "])(?=[^" + "".join(URDU_PUNCTUATIONS) + "0-9 \n])",
+        flags=re.U | re.M | re.I)
+REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE = re.compile(r'\s+([' + "".join(URDU_PUNCTUATIONS) + '])',
+                                                 flags=re.U | re.M | re.I)
 
 
 def punctuations_space(text: str) -> str:
@@ -65,12 +60,10 @@ def punctuations_space(text: str) -> str:
 
 # Add spaces before|after english characters and urdu words
 # ikramسالہ  , abفیصد
-SPACE_BEFORE_ENG_CHAR_RE = re.compile(
-    r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[a-zA-Z])",
-    flags=re.U | re.M | re.I)
-SPACE_AFTER_ENG_CHAR_RE = re.compile(
-    r"(?<=[a-zA-Z])(?=[" + "".join(URDU_ALL_CHARACTERS) + "])",
-    flags=re.U | re.M | re.I)
+SPACE_BEFORE_ENG_CHAR_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[a-zA-Z])",
+                                      flags=re.U | re.M | re.I)
+SPACE_AFTER_ENG_CHAR_RE = re.compile(r"(?<=[a-zA-Z])(?=[" + "".join(URDU_ALL_CHARACTERS) + "])",
+                                     flags=re.U | re.M | re.I)
 
 
 def english_characters_space(text: str) -> str:
@@ -89,8 +82,7 @@ def english_characters_space(text: str) -> str:
     return text
 
 
-DIACRITICS_RE = re.compile(f'[{"".join(URDU_DIACRITICS)}]',
-                           flags=re.U | re.M | re.I)
+DIACRITICS_RE = re.compile(f'[{"".join(URDU_DIACRITICS)}]', flags=re.U | re.M | re.I)
 
 
 def remove_diacritics(text: str) -> str:
@@ -104,134 +96,3 @@ def remove_diacritics(text: str) -> str:
         str: returns a ``str`` object containing normalized text.
     """
     return DIACRITICS_RE.sub('', text)
-
-
-WORDS_SPACE: Dict[str, str] = {"کردیا": "کر دیا",
-                               "کردی": "کر دی",
-                               "ہوگیا": "ہو گیا",
-                               "کرگئے": "کر گئے",
-                               "جاسکتے": "جا سکتے",
-                               "کرلیا": "کر لیا",
-                               "ہوچکا": "ہو چکا",
-                               "کرادی": "کرا دی",
-                               "ہوگئی": "ہو گئی",
-                               "جارہا": "جا رہا",
-                               "ہوجاتے": "ہو جاتے",
-                               "ہوچکی": "ہو چکی",
-                               "اڑادیا": "اڑا دیا",
-                               "کردیئے": "کر دیئے",
-                               "ہوگئے": "ہو گئے",
-                               "ہوجائے": "ہو جائے",
-                               "کردیے": "کر دیے",
-                               "کرسکیں": "کر سکیں",
-                               "بتایاہے": "بتایا ہے",
-                               "رہاہے": "رہا ہے",
-                               "ہےکہ": "ہے کہ",
-                               "بالاترہے": "بالاتر ہے",
-                               "کہاہے": "کہا ہے",
-                               "جاتاہے": "جاتا ہے",
-                               "بلایاہے": "بلایا ہے",
-                               "جارہاہے": "جا رہا ہے",
-                               "دیاہے": "دیا ہے",
-                               "کررہے": "کر رہے",
-                               "ہوگا": "ہو گا",
-                               "کیاتھا": "کیا تھا",
-                               "کرلی۔": "کر لی۔",
-                               "کیاگیا": "کیا گیا",
-                               "گیاتھا": "گیا تھا",
-                               "کےلئے": "کے لئے",
-                               "کےلئے۔": "کے لئے۔",
-                               "لگادیا": "لگا دیا",
-                               "ہوجانا": "ہو جانا",
-                               "گا۔انہوں": "گا۔ انہوں",
-                               "کردئیے": "کر دئیے",
-                               "کیاجائے": "کیا جائے",
-                               "آرہا": "آ رہا",
-                               "دیاگیا": "دیا گیا",
-                               "ہے۔آپ": "ہے۔ آپ",
-                               "کاکہناہے": "کا کہنا ہے",
-                               "ہے۔لیکن": "ہے۔ لیکن",
-                               "بتایاگیا": "بتایا گیا",
-                               "ہے۔ایسے": "ہے۔ ایسے",
-                               "ہے۔ترجمان": "ہے۔ ترجمان",
-                               "دیکھاجاسکتاہے": "دیکھا جا سکتا ہے",
-                               "کامقدمہ": "کا مقدمہ",
-                               "نظرآنے": "نظر آنے",
-                               "گی۔فلم": "گی۔ فلم",
-                               "بوگی": "بو گی",
-                               "چھوڑدیں": "چھوڑ دیں",
-                               "گا۔ماہرین": "گا۔ ماہرین",
-                               "بتاسکتی": "بتا سکتی",
-                               "ہے۔بہت": "ہے۔ بہت",
-                               "تھے۔جب": "تھے۔ جب",
-                               "ہیں۔شاہ": "ہیں۔ شاہ",
-                               "گیا۔محکمہ": "گیا۔ محکمہ",
-                               "تھے۔لیکن": "تھے۔ لیکن",
-                               "تھی۔ذرائع": "تھی۔ ذرائع",
-                               "کرلیں۔اس": "کرلیں۔ اس",
-                               "ہواہے۔": "ہوا ہے۔",
-                               "کرارہے": "کرا رہے",
-                               "ہے۔کئی": "ہے۔ کئی",
-                               "کرناہو": "کرنا ہو",
-                               "کرسکتے": "کر سکتے",
-                               "جارہی": "جا رہی",
-                               "کہناتھا": "کہنا تھا",
-                               "کےلیے": "کے لیے",
-                               "کاکہناتھاکہ": "کا کہنا تھا کہ",
-                               "جاکر": "جا کر",
-                               "کیاہے": "کیا ہے",
-                               "کودیا": "کو دیا",
-                               "توحکومت": "تو حکومت",
-                               "کومعاف": "کو معاف",
-                               "کوخبردارکیا": "کو خبردار کیا",
-                               "کوجانے": "کو جانے",
-                               "کواپنے": "کو اپنے",
-                               "اگرکوئی": "اگر کوئی",
-                               "کوپھانسی": "کو پھانسی",
-                               "کواستعمال": "کو استعمال",
-                               "ہےجب": "ہے جب",
-                               "رکھاگیا": "رکھا گیا",
-                               "دکھایاگیا": "دکھایا گیا",
-                               "کےنتیجے": "کے نتیجے",
-                               "کیاجاسکتا": "کیا جا سکتا",
-                               "ہے ۔": "ہے۔",
-                               "ہیں ۔": "ہیں۔",
-                               "گے ۔": "گے۔",
-                               "تھا ۔": "تھا۔",
-                               "گیا ۔": "گیا۔",
-                               "تھے ۔": "تھے۔",
-                               "تھی ۔": "تھی۔",
-                               "گئے ۔": "گئے۔",
-                               "گا ۔": "گا۔",
-                               "دیا ۔": "دیا۔",
-                               "کیا ۔": "کیا۔",
-                               "تھاکہ": "تھا کہ",
-                               "کہاکہ": "کہا کہ",
-                               "کےبعد": "کے بعد",
-                               "کرکے": "کر کے",
-                               "گیاہے": "گیا ہے",
-                               "کررہا": "کر رہا",
-                               "کاکہنا": "کا کہنا",
-                               "ہوتاہے": "ہوتا ہے",
-                               "گئےہیں": "گئے ہیں",
-                               "آگئے": "آ گئے",
-                               "ہوجاتی": "ہو جاتی",
-                               "جاسکتا": "جا سکتا",
-                               "کرناہے": "کرنا ہے",
-                               }
-
-
-def fix_join_words(text: str) -> str:
-    """
-    Replace all join ``urdu`` words with separate words
-
-    Args:
-        text (str): raw ``urdu`` text
-
-    Returns:
-        str: returns a ``str`` object containing normalized text.
-    """
-    for key, value in WORDS_SPACE.items():
-        text = text.replace(key, value)
-
-    return text
--- urduhack/preprocess/util.py
+++ urduhack/preprocess/util.py
@@ -15,66 +15,60 @@ CURRENCIES = {'$': 'USD', 'zł': 'PLN', '£': 'GBP', '¥': 'JPY', '฿': 'THB',
 EMAIL_REGEX = re.compile(
         r"(?:^|(?<=[^\w@.)]))([\w+-](\.(?!\.))?)*?[\w+-]@(?:\w-?)*?\w+(\.([a-z]{2,})){1,3}(?:$|(?=\b))",
         flags=re.IGNORECASE | re.UNICODE)
-PHONE_REGEX = re.compile(
-        r'(?:^|(?<=[^\w)]))(\+?1[ .-]?)?(\(?\d{3}\)?[ .-]?)?(\d{3}[ .-]?\d{4})(\s?(?:ext\.?'
-        r'|[#x-])\s?\d{2,6})?(?:$|(?=\W))')
-NUMBERS_REGEX = re.compile(
-        r'(?:^|(?<=[^\w,.]))[+–-]?(([1-9]\d{0,2}(,\d{3})+(\.\d*)?)|([1-9]\d{0,2}([ .]\d{3})+(,\d*)?)'
-        r'|(\d*?[.,]\d+)|\d+)(?:$|(?=\b))')
+PHONE_REGEX = re.compile(r'(?:^|(?<=[^\w)]))(\+?1[ .-]?)?(\(?\d{3}\)?[ .-]?)?(\d{3}[ .-]?\d{4})(\s?(?:ext\.?'
+                         r'|[#x-])\s?\d{2,6})?(?:$|(?=\W))')
+NUMBERS_REGEX = re.compile(r'(?:^|(?<=[^\w,.]))[+–-]?(([1-9]\d{0,2}(,\d{3})+(\.\d*)?)|([1-9]\d{0,2}([ .]\d{3})+(,\d*)?)'
+                           r'|(\d*?[.,]\d+)|\d+)(?:$|(?=\b))')
 CURRENCY_REGEX = re.compile('({})+'.format('|'.join(re.escape(c) for c in CURRENCIES)))
 LINEBREAK_REGEX = re.compile(r'((\r\n)|[\n\v])+')
 NONBREAKING_SPACE_REGEX = re.compile(r'(?!\n)\s+')
-URL_REGEX = re.compile(
-        r"(?:^|(?<![\w/.]))"
-        # protocol identifier
-        # r"(?:(?:https?|ftp)://)"  <-- alt?
-        r"(?:(?:https?://|ftp://|www\d{0,3}\.))"
-        # user:pass authentication
-        r"(?:\S+(?::\S*)?@)?"
-        r"(?:"
-        # IP address exclusion
-        # private & local networks
-        r"(?!(?:10|127)(?:\.\d{1,3}){3})"
-        r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
-        r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
-        # IP address dotted notation octets
-        # excludes loopback network 0.0.0.0
-        # excludes reserved space >= 224.0.0.0
-        # excludes network & broadcast addresses
-        # (first & last IP address of each class)
-        r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
-        r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
-        r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
-        r"|"
-        # host name
-        r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
-        # domain name
-        r"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
-        # TLD identifier
-        r"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
-        r")"
-        # port number
-        r"(?::\d{2,5})?"
-        # resource path
-        r"(?:/\S*)?"
-        r"(?:$|(?![\w?!+&/]))",
-        flags=re.UNICODE | re.IGNORECASE)  # source: https://gist.github.com/dperini/729294
-SHORT_URL_REGEX = re.compile(
-        r"(?:^|(?<![\w/.]))"
-        # optional scheme
-        r"(?:(?:https?://)?)"
-        # domain
-        r"(?:\w-?)*?\w+(?:\.[a-z]{2,12}){1,3}"
-        r"/"
-        # hash
-        r"[^\s.,?!'\"|+]{2,12}"
-        r"(?:$|(?![\w?!+&/]))",
-        flags=re.IGNORECASE)
+URL_REGEX = re.compile(r"(?:^|(?<![\w/.]))"
+                       # protocol identifier
+                       # r"(?:(?:https?|ftp)://)"  <-- alt?
+                       r"(?:(?:https?://|ftp://|www\d{0,3}\.))"
+                       # user:pass authentication
+                       r"(?:\S+(?::\S*)?@)?"
+                       r"(?:"
+                       # IP address exclusion
+                       # private & local networks
+                       r"(?!(?:10|127)(?:\.\d{1,3}){3})"
+                       r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+                       r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+                       # IP address dotted notation octets
+                       # excludes loopback network 0.0.0.0
+                       # excludes reserved space >= 224.0.0.0
+                       # excludes network & broadcast addresses
+                       # (first & last IP address of each class)
+                       r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+                       r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+                       r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+                       r"|"
+                       # host name
+                       r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+                       # domain name
+                       r"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+                       # TLD identifier
+                       r"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
+                       r")"
+                       # port number
+                       r"(?::\d{2,5})?"
+                       # resource path
+                       r"(?:/\S*)?"
+                       r"(?:$|(?![\w?!+&/]))",
+                       flags=re.UNICODE | re.IGNORECASE)  # source: https://gist.github.com/dperini/729294
+SHORT_URL_REGEX = re.compile(r"(?:^|(?<![\w/.]))"
+                             # optional scheme
+                             r"(?:(?:https?://)?)"
+                             # domain
+                             r"(?:\w-?)*?\w+(?:\.[a-z]{2,12}){1,3}"
+                             r"/"
+                             # hash
+                             r"[^\s.,?!'\"|+]{2,12}"
+                             r"(?:$|(?![\w?!+&/]))",
+                             flags=re.IGNORECASE)
 
-PUNCTUATION_TRANSLATE_UNICODE = dict.fromkeys(
-        (i for i in range(sys.maxunicode)
-         if unicodedata.category(chr(i)).startswith('P')),
-        u' ')
+PUNCTUATION_TRANSLATE_UNICODE = dict.fromkeys((i for i in range(sys.maxunicode)
+                                               if unicodedata.category(chr(i)).startswith('P')), u' ')
 
 
 def normalize_whitespace(text: str):
