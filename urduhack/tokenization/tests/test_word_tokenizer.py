49715ae833b62d228171a28e212ceaa2f44f75a9
fahdrazavi <fahdrazavi@gmail.com>
2019-08-26 14:42:21 +0500

    [tokenizer] Neural word tokenizer added

--- urduhack/tokenization/tests/test_word_tokenizer.py
+++ urduhack/tokenization/tests/test_word_tokenizer.py
@@ -1,6 +1,10 @@
 from ..word_tokenizer import UrduWordTokenizer
+
+
 def test():
     tokenizer = UrduWordTokenizer()
     print(tokenizer.tokenize_words('میڈیاکے نمائندوں نےنیپال میں میڈیاکی صورت حال سےمتعلق آگاہ کیا'))
+
+
 if __name__ == "__main__":
     test()
