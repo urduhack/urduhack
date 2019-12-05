76863b2d0743f9eccfaa6720843e215152fde9bd
muhammadfahid51 <muhammad.fahid@arbisoft.com>
2019-12-05 10:34:35 +0500

    added keras_tokenizer_predict to the init file

--- urduhack/__init__.py
+++ urduhack/__init__.py
@@ -2,5 +2,6 @@
 """Project Entry point"""
 from .about import __version__
 from .normalization import normalize
+from .tokenization.tokenizer import keras_tokenizer_predict
 
-__all__ = ["__version__", "normalize"]
+__all__ = ["__version__", "normalize", "keras_tokenizer_predict"]
