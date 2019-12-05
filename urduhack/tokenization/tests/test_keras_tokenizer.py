293f471fc46d4680e9d9fface96ff431fc04838a
Ikram Ali <mrikram1989@gmail.com>
2019-12-05 18:42:04 +0500

    [tokenizer] bug fixes and code refactoring.

--- urduhack/tokenization/tests/test_keras_tokenizer.py
+++
@@ -1,9 +0,0 @@
-# coding: utf8
-"""test case"""
-from urduhack.tokenization.tokenizer import MODEL_PATH
-from pathlib import Path
-
-
-def test_keras_tokenizer_predict():
-    """Test Case"""
-    assert MODEL_PATH == str(Path.home()) + "urduhack/models/word_tokenizer.h5"
