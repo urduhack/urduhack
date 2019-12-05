6dadfb804d009be63c5e638d4df3a07de15ac45a
muhammadfahid51 <muhammad.fahid@arbisoft.com>
2019-12-05 11:10:38 +0500

    added test case "test_keras_tokenizer" for tokenizer

--- urduhack/tokenization/tests/test_keras_tokenizer.py
+++ urduhack/tokenization/tests/test_keras_tokenizer.py
@@ -1,10 +1,9 @@
 # coding: utf8
 """test case"""
-from ..tokenizer import *
+from urduhack.tokenization.tokenizer import MODEL_PATH
 from pathlib import Path
 
 
 def test_keras_tokenizer_predict():
     """Test Case"""
-
     assert MODEL_PATH == str(Path.home()) + "urduhack/models/word_tokenizer.h5"
