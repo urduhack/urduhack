8320c9a94983ba5fac861618c2d90700cd41c8e7
Ikram Ali <mrikram1989@gmail.com>
2019-12-16 11:29:00 +0500

    [stopwords] Stopwords list added.

--- urduhack/__init__.py
+++ urduhack/__init__.py
@@ -2,5 +2,6 @@
 """Project Entry point"""
 from .about import __version__
 from .normalization import normalize
+from .stop_words import STOP_WORDS
 
-__all__ = ["__version__", "normalize"]
+__all__ = ["__version__", "normalize", "STOP_WORDS"]
