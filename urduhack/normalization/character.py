ce4d52f04cc5c88084e5e888b8891b8cc80a38b0
Ikram Ali <mrikram1989@gmail.com>
2020-03-17 19:35:37 +0500

    [normalization] Add private constants.

--- urduhack/normalization/character.py
+++ urduhack/normalization/character.py
@@ -12,10 +12,10 @@ from ..urdu_characters import URDU_ALL_CHARACTERS, URDU_PUNCTUATIONS, URDU_DIACR
 
 # Add spaces before|after numeric number and urdu words
 # 18سالہ  , 20فیصد
-EXCEPT_HAMZA = list(filter(lambda c: c != '\u0621', URDU_ALL_CHARACTERS))
-SPACE_BEFORE_DIGITS_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[0-9])",
-                                    flags=re.U | re.M | re.I)
-SPACE_AFTER_DIGITS_RE = re.compile(r"(?<=[0-9])(?=[" + "".join(EXCEPT_HAMZA) + "])", flags=re.U | re.M | re.I)
+_EXCEPT_HAMZA = list(filter(lambda c: c != '\u0621', URDU_ALL_CHARACTERS))
+_SPACE_BEFORE_DIGITS_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[0-9])",
+                                     flags=re.U | re.M | re.I)
+_SPACE_AFTER_DIGITS_RE = re.compile(r"(?<=[0-9])(?=[" + "".join(_EXCEPT_HAMZA) + "])", flags=re.U | re.M | re.I)
 # Issue to be resolved: Words like کیجئے and کیجیے appear in the same context but they have different unicodes.
 # We cannot merge them neither can we have them separately. Because if we decompose ئ,
 # we get unicode that are not available in our unicode list.
@@ -137,18 +137,18 @@ def digits_space(text: str) -> str:
     Returns:
         str: returns a ``str`` object containing normalized text.
     """
-    text = SPACE_BEFORE_DIGITS_RE.sub(' ', text)
-    text = SPACE_AFTER_DIGITS_RE.sub(' ', text)
+    text = _SPACE_BEFORE_DIGITS_RE.sub(' ', text)
+    text = _SPACE_AFTER_DIGITS_RE.sub(' ', text)
 
     return text
 
 
 # Add spaces after ., if there is number then not Ex (9.00)
-SPACE_AFTER_PUNCTUATIONS_RE = re.compile(
+_SPACE_AFTER_PUNCTUATIONS_RE = re.compile(
     r"(?<=[" + "".join(URDU_PUNCTUATIONS) + "])(?=[^" + "".join(URDU_PUNCTUATIONS) + "0-9 \n])",
     flags=re.U | re.M | re.I)
-REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE = re.compile(r'\s+([' + "".join(URDU_PUNCTUATIONS) + '])',
-                                                 flags=re.U | re.M | re.I)
+_REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE = re.compile(r'\s+([' + "".join(URDU_PUNCTUATIONS) + '])',
+                                                  flags=re.U | re.M | re.I)
 
 
 def punctuations_space(text: str) -> str:
@@ -160,17 +160,17 @@ def punctuations_space(text: str) -> str:
     Returns:
         str: returns a ``str`` object containing normalized text.
     """
-    text = SPACE_AFTER_PUNCTUATIONS_RE.sub(' ', text)
-    text = REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE.sub(r'\1', text)
+    text = _SPACE_AFTER_PUNCTUATIONS_RE.sub(' ', text)
+    text = _REMOVE_SPACE_BEFORE_PUNCTUATIONS_RE.sub(r'\1', text)
     return text
 
 
 # Add spaces before|after english characters and urdu words
 # ikramسالہ  , abفیصد
-SPACE_BEFORE_ENG_CHAR_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[a-zA-Z])",
+_SPACE_BEFORE_ENG_CHAR_RE = re.compile(r"(?<=[" + "".join(URDU_ALL_CHARACTERS) + "])(?=[a-zA-Z])",
+                                       flags=re.U | re.M | re.I)
+_SPACE_AFTER_ENG_CHAR_RE = re.compile(r"(?<=[a-zA-Z])(?=[" + "".join(URDU_ALL_CHARACTERS) + "])",
                                       flags=re.U | re.M | re.I)
-SPACE_AFTER_ENG_CHAR_RE = re.compile(r"(?<=[a-zA-Z])(?=[" + "".join(URDU_ALL_CHARACTERS) + "])",
-                                     flags=re.U | re.M | re.I)
 
 
 def english_characters_space(text: str) -> str:
@@ -182,13 +182,13 @@ def english_characters_space(text: str) -> str:
     Returns:
         str: returns a ``str`` object containing normalized text.
     """
-    text = SPACE_BEFORE_ENG_CHAR_RE.sub(' ', text)
-    text = SPACE_AFTER_ENG_CHAR_RE.sub(' ', text)
+    text = _SPACE_BEFORE_ENG_CHAR_RE.sub(' ', text)
+    text = _SPACE_AFTER_ENG_CHAR_RE.sub(' ', text)
 
     return text
 
 
-DIACRITICS_RE = re.compile(f'[{"".join(URDU_DIACRITICS)}]', flags=re.U | re.M | re.I)
+_DIACRITICS_RE = re.compile(f'[{"".join(URDU_DIACRITICS)}]', flags=re.U | re.M | re.I)
 
 
 def remove_diacritics(text: str) -> str:
@@ -200,4 +200,4 @@ def remove_diacritics(text: str) -> str:
     Returns:
         str: returns a ``str`` object containing normalized text.
     """
-    return DIACRITICS_RE.sub('', text)
+    return _DIACRITICS_RE.sub('', text)
