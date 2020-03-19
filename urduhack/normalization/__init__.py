0c341522f24d7fc31e0bbeae9aa2e864e57b9d7b
Ikram Ali <mrikram1989@gmail.com>
2020-03-19 13:59:46 +0500

    [task] bug fixed.

--- urduhack/normalization/__init__.py
+++ urduhack/normalization/__init__.py
@@ -27,30 +27,9 @@ This module provides the following functionality:
 """
 
 from .character import normalize_characters, normalize_combine_characters, punctuations_space, digits_space, \
-    remove_diacritics, english_characters_space
+    remove_diacritics, english_characters_space, normalize
 
 __all__ = ["normalize_characters", "normalize_combine_characters",
            "english_characters_space",
            "punctuations_space", "digits_space", "remove_diacritics",
            "normalize"]
-
-
-def normalize(text: str) -> str:
-    """
-    To normalize some text, all you need to do pass ``unicode`` text. It will return a ``str``
-    with normalized characters both single and combined, proper spaces after digits and punctuations
-    and diacritics removed.
-
-    Args:
-        text (str): raw ``unicode`` Urdu text
-
-    Returns:
-        str: normalized urdu text
-    """
-    text = normalize_characters(text)
-    text = normalize_combine_characters(text)
-    text = digits_space(text)
-    text = punctuations_space(text)
-    text = remove_diacritics(text)
-    text = english_characters_space(text)
-    return text
