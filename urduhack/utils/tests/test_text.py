86f469b63ef5d9d0f6498cc7d9b5e9581444c7d1
Ikram Ali <mrikram1989@gmail.com>
2020-03-21 13:29:53 +0500

    [task] bug fixed.

--- urduhack/utils/tests/test_text.py
+++ urduhack/utils/tests/test_text.py
@@ -1,6 +1,14 @@
 """Test Case"""
 
-from ..text import get_code_point
+from ..text import get_code_point, load_vocab
+
+
+def test_load_vocab(tmpdir):
+    """Test Case"""
+    assert len(load_vocab()) == 2
+    char2idx, idx2char = load_vocab()
+    assert isinstance(char2idx, dict)
+    assert isinstance(idx2char, dict)
 
 
 def test_get_code_point():
--- urduhack/utils/text.py
+++ urduhack/utils/text.py
@@ -1,6 +1,36 @@
 # coding: utf8
 """Text Utils"""
 
+from typing import Tuple
+
+from urduhack.urdu_characters import URDU_ALL_CHARACTERS
+
+_vocab_sources = {
+    "urdu": "".join(URDU_ALL_CHARACTERS),
+    "english": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    "math": "0123456789"
+}
+
+
+def load_vocab(extra_vocab: str = None) -> Tuple[dict, dict]:
+    """
+    Complete vocab used by models. Maps characters to integers and vice versa
+
+    Args:
+        extra_vocab (str): extra vocab text
+    Returns:
+        tuple: Two dictionaries containing character to integer mapping and integer to character mapping
+    """
+    vocab: str = ""
+    for value in _vocab_sources.values():
+        vocab += value
+    if extra_vocab is not None:
+        vocab += extra_vocab
+    char2idx = {char: idx for idx, char in enumerate(vocab)}
+    idx2char = {idx: char for idx, char in enumerate(vocab)}
+
+    return char2idx, idx2char
+
 
 def get_code_point(char: str) -> str:
     """
