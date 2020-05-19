a69737ed9a94ec1c02913f9fa0ea8abc08fcdb25
Ikram Ali <mrikram1989@gmail.com>
2020-05-19 20:30:36 +0500

    [requirement] packages removed.

--- urduhack/__init__.py
+++ urduhack/__init__.py
@@ -3,5 +3,6 @@
 from .about import __version__
 from .normalization import normalize
 from .stop_words import STOP_WORDS
+from .utils.resources import download
 
-__all__ = ["__version__", "normalize", "STOP_WORDS"]
+__all__ = ["__version__", "normalize", "STOP_WORDS", "download"]
