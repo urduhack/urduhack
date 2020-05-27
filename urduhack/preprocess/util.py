a831010880863abb305a1f59802357595e8bd415
Ikram Ali <mrikram1989@gmail.com>
2020-05-27 11:25:43 +0500

    [preprocess] remove_punctuation whitespace bug fixed. fix #111

--- urduhack/preprocess/util.py
+++ urduhack/preprocess/util.py
@@ -69,9 +69,6 @@ _SHORT_URL_RE = re.compile(r"(?:^|(?<![\w/.]))"
                            r"(?:$|(?![\w?!+&/]))",
                            flags=re.IGNORECASE)
 
-PUNCTUATION_TRANSLATE_UNICODE = dict.fromkeys((i for i in range(sys.maxunicode)
-                                               if unicodedata.category(chr(i)).startswith('P')), u' ')
-
 
 def normalize_whitespace(text: str):
     """
@@ -159,9 +156,13 @@ def replace_currency_symbols(text: str, replace_with=None):
     return _CURRENCY_RE.sub(replace_with, text)
 
 
+PUNCTUATION_TRANSLATE_UNICODE = dict.fromkeys((i for i in range(sys.maxunicode)
+                                               if unicodedata.category(chr(i)).startswith('P')), '')
+
+
 def remove_punctuation(text: str, marks=None) -> str:
     """
-    Remove punctuation from ``text`` by replacing all instances of ``marks`` with whitespace.
+    Remove punctuation from ``text`` by removing all instances of ``marks``.
 
     Args:
         text (str): raw text
@@ -174,9 +175,14 @@ def remove_punctuation(text: str, marks=None) -> str:
         When ``marks=None``, Python's built-in :meth:`str.translate()` is
         used to remove punctuation; otherwise, a regular expression is used
         instead. The former's performance is about 5-10x faster.
+    Examples:
+        >>> from urduhack.preprocess import remove_punctuation
+        >>> output = remove_punctuation("کر ؟ سکتی ہے۔")
+        کر سکتی ہے
+
     """
     if marks:
-        return re.sub('[{}]+'.format(re.escape(marks)), ' ', text, flags=re.UNICODE)
+        return re.sub('[{}]+'.format(re.escape(marks)), '', text, flags=re.UNICODE)
 
     return text.translate(PUNCTUATION_TRANSLATE_UNICODE)
 
