43ddf068baf377ea9c50cfe46933eb07e7111916
Ikram Ali <mrikram1989@gmail.com>
2020-06-13 17:53:18 +0500

    [pipeline] pipeline added.

---
+++ urduhack/pipeline/core.py
@@ -0,0 +1,42 @@
+# coding: utf8
+"""
+Pipeline that runs tokenize
+"""
+from typing import Dict, List
+
+from urduhack.pipeline.parsers.normalize import NormalizeParser
+from urduhack.pipeline.parsers.tokenize import TokenizeParser
+
+NORMALIZE: str = 'normalize'
+TOKENIZE: str = 'tokenize'
+
+PIPELINE_NAMES: List = [NORMALIZE, TOKENIZE]
+REGISTERED_PARSERS: Dict = {NORMALIZE: NormalizeParser, TOKENIZE: TokenizeParser}
+
+
+class Pipeline:
+    """Pipeline to process"""
+
+    def __init__(self):
+        """Pass"""
+
+        self.parsers = {}
+        for item in REGISTERED_PARSERS.keys():
+            self.parsers[item] = REGISTERED_PARSERS[item](config={}, pipeline=self)
+
+    @property
+    def loaded_parsers(self):
+        """Return all currently loaded parsers in execution order."""
+        return [self.parsers[parser_name] for parser_name in PIPELINE_NAMES if self.parsers.get(parser_name)]
+
+    def parse(self, doc):
+        """Tst"""
+        for parser_name in PIPELINE_NAMES:
+            if self.parsers.get(parser_name):
+                doc = self.parsers[parser_name].parse(doc)
+        return doc
+
+    def __call__(self, doc):
+        assert any([isinstance(doc, str), isinstance(doc, list)]), 'input should be either str or list'
+        doc = self.parse(doc)
+        return doc
---
+++ urduhack/pipeline/parser.py
@@ -0,0 +1,47 @@
+# coding: utf8
+"""
+Base classes for parser
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+    """ Base class for all parsers"""
+
+    def __init__(self, config, pipeline):
+        # overall config for the processor
+        self._config = None
+        # pipeline building this processor (presently parsers are only meant to exist in one pipeline)
+        self._pipeline = pipeline
+        # UD model resources, set up is processor specific
+        self._pretrain = None
+        self._trainer = None
+        self._vocab = None
+        self._set_up(config)
+        # run set up process
+        # build the final config for the processor
+        self._set_up_final_config(config)
+
+    @abstractmethod
+    def parse(self, document):
+        """ Process a Document.  This is the main method of a processor. """
+        pass
+
+    @abstractmethod
+    def _set_up(self, config):
+        pass
+
+    @property
+    def config(self):
+        """ Configurations for the processor """
+        return self._config
+
+    @property
+    def pipeline(self):
+        """ The pipeline that this processor belongs to """
+        return self._pipeline
+
+    def _set_up_final_config(self, config):
+        """ Finalize the configurations for this processor, based off of values from a UD model. """
+        self._config = config
