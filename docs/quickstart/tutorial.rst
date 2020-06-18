161bece9fcc56fecd651218cf6897c7bb2f29652
muhammadfahid <muhammad.fahid@arbisoft.com>
2020-06-18 22:50:45 +0500

    [docs] updated tests and added pipeline tutorial

--- docs/quickstart/tutorial.rst
+++ docs/quickstart/tutorial.rst
@@ -4,18 +4,18 @@ Tutorial
 How to explore CoNLL-U Format
 -----------------------------
 We aspire to maintain data for all the tasks in CoNNL-U format. CoNLL-U format holds sentence and token level data along with their
-attributes. Below we will show how to use urduhack's :py:mod:`~urduhack.CoNLL` module.::
+attributes. Below we will show how to use urduhack's :py:mod:`~urduhack.CoNLL` module. ::
 
 
     >>> from urduhack import CoNLL
 
-To iterate over sentences in CoNLL-U format we will use :py:func:`~urduhack.CoNLL.iter_string` function.::
+To iterate over sentences in CoNLL-U format we will use :py:func:`~urduhack.CoNLL.iter_string` function. ::
 
     >>> from urduhack.conll.tests.test_parser import CONLL_SENTENCE
+
+It will yield a sentence in proper CoNLL-U format from which we can extract sentence level and token level attributes. ::
+
     >>> text_iterator = CoNLL.iter_string(CONLL_SENTENCE)
-
-It will yield a sentence in proper CoNLL-U format from which we can extract sentence level and token level attributes.::
-
     >>> for sentence in text_iterator:
             sent_meta, tokens = sentence
             print(f"Sentence ID: {sent_meta['sent_id']}")
@@ -31,7 +31,7 @@ It will yield a sentence in proper CoNLL-U format from which we can extract sent
     {'id': '5', 'text': 'ہےں', 'lemma': 'ہے', 'upos': 'AUX', 'xpos': 'VAUX', 'feats': 'Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin', 'head': '4', 'deprel': 'aux', 'deps': '_', 'misc': 'SpaceAfter=No|Vib=ہے|Tam=hE|ChunkId=VGF|ChunkType=child'}
     {'id': '6', 'text': '۔', 'lemma': '۔', 'upos': 'PUNCT', 'xpos': 'SYM', 'feats': '_', 'head': '4', 'deprel': 'punct', 'deps': '_', 'misc': 'ChunkId=VGF|ChunkType=child'}
 
-To load a file in ConLL-U format, we will use :py:func:`urduhack.CoNLL.load_file` function.::
+To load a file in ConLL-U format, we will use :py:func:`urduhack.CoNLL.load_file` function. ::
 
     >>> sentences = ConLL.load_file("urdu_text.conll")
     >>> for sentence in sentences:
@@ -48,3 +48,68 @@ To load a file in ConLL-U format, we will use :py:func:`urduhack.CoNLL.load_file
     {'id': '4', 'text': 'ہوئے', 'lemma': 'ہو', 'upos': 'VERB', 'xpos': 'VM', 'feats': 'Aspect=Perf|Number=Plur|Person=2|Polite=Form|VerbForm=Part|Voice=Act', 'head': '0', 'deprel': 'root', 'deps': '_', 'misc': 'Vib=یا|Tam=yA|ChunkId=VGF|ChunkType=head|Stype=declarative'}
     {'id': '5', 'text': 'ہےں', 'lemma': 'ہے', 'upos': 'AUX', 'xpos': 'VAUX', 'feats': 'Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin', 'head': '4', 'deprel': 'aux', 'deps': '_', 'misc': 'SpaceAfter=No|Vib=ہے|Tam=hE|ChunkId=VGF|ChunkType=child'}
     {'id': '6', 'text': '۔', 'lemma': '۔', 'upos': 'PUNCT', 'xpos': 'SYM', 'feats': '_', 'head': '4', 'deprel': 'punct', 'deps': '_', 'misc': 'ChunkId=VGF|ChunkType=child'}
+
+How to use Pipeline Module
+--------------------------
+Pipeline is a special module in urduhack. It's importance can be realized by the fact that it performs operation at **Document**,
+**Sentence** and **Token** level. We can convert a document to sentence and a sentence into tokens in one go using the pipeline
+module. After that we can run models or any other operation at the document, sentence and token levels.
+Now we will go into these steps one by on.
+
+**Document**
+
+We can get the document using pipeline module. ::
+
+    >>> from urduhack.pipeline import Pipeline
+    >>> nlp = Pipeline(
+    >>> text = """
+    گزشتہ ایک روز کے دوران کورونا کے سبب 118 اموات ہوئیں جس کے بعد اموات کا مجموعہ 3 ہزار 93 ہوگیا ہے۔
+    سب سے زیادہ اموات بھی پنجاب میں ہوئی ہیں جہاں ایک ہزار 202 افراد جان کی بازی ہار چکے ہیں۔
+    سندھ میں 916، خیبر پختونخوا میں 755، اسلام آباد میں 94، گلگت بلتستان میں 18، بلوچستان میں 93 اور ا?زاد کشمیر میں 15 افراد کورونا وائرس سے جاں بحق ہو چکے ہیں۔
+    """
+    >>> doc = nlp(text)
+    >>> print(doc.text)
+    گزشتہ ایک روز کے دوران کورونا کے سبب 118 اموات ہوئیں جس کے بعد اموات کا مجموعہ 3 ہزار 93 ہوگیا ہے۔
+    سب سے زیادہ اموات بھی پنجاب میں ہوئی ہیں جہاں ایک ہزار 202 افراد جان کی بازی ہار چکے ہیں۔
+    سندھ میں 916، خیبر پختونخوا میں 755، اسلام آباد میں 94، گلگت بلتستان میں 18، بلوچستان میں 93 اور ا?زاد کشمیر میں 15 افراد کورونا وائرس سے جاں بحق ہو چکے ہیں۔
+
+**Sentence**
+
+Now to get the sentences from the Document. ::
+
+    >>> for sentence in doc.sentences:
+            print(sentence.text)
+    گزشتہ ایک روز کے دوران کورونا کے سبب 118 اموات ہوئیں جس کے بعد اموات کا مجموعہ 3 ہزار 93 ہوگیا ہے
+    سب سے زیادہ اموات بھی پنجاب میں ہوئی ہیں
+    جہاں ایک ہزار 202 افراد جان کی بازی ہار چکے ہیں۔
+    سندھ میں 916، خیبر پختونخوا میں 755، اسلام آباد میں 94، گلگت بلتستان میں 18، بلوچستان میں 93 اور ا?زاد کشمیر میں 15 افراد کورونا وائرس سے جاں بحق ہو چکے ہی
+    گزشتہ ایک روز کے دوران کورونا کے سبب 118 اموات ہوئیں جس کے بعد اموات کا مجموعہ 3 ہزار 93 ہوگیا ہے
+
+**Word**
+
+To get words from sentence. ::
+
+    >>> for word in sentence.words:
+            print(word.text)
+     گزشتہ
+     ایک
+     روز
+     کے
+     دوران
+     کورونا
+     کے
+     سبب
+     118
+     اموات
+     ہوئیںجس
+     کے
+     بعد
+     اموات
+     کا
+     مجموعہ
+     3
+     ہزار
+     93
+     ہو
+     گیا
+     ہے۔
--- urduhack/core/unit/tests/test_token.py
+++ urduhack/core/unit/tests/test_token.py
@@ -1,11 +1,10 @@
 # coding: utf8
 """
-Test cases for sentence
+Test cases for Tokens
 """
 from urduhack import CoNLL
-from urduhack.core.unit.document import Document
 from urduhack.core.unit.sentence import Sentence
-from urduhack.core.unit.token import Token, Word
+from urduhack.core.unit.token import Word, Token
 
 CONLL_SENTENCE = """
 # sent_id = test-s13
@@ -19,390 +18,38 @@ CONLL_SENTENCE = """
 """
 
 
-def test_token_init_from_misc(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
-            assert token.init_from_misc() is None
-
-
-def test_token_id(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
+def test_token():
+    for sentence in CoNLL.iter_string(CONLL_SENTENCE):
+        sent_meta, tokens = sentence
+        for token in tokens:
+            token = Token(token)
+            assert isinstance(token, Token)
             assert isinstance(token.id, str)
-            token.id = "1"
-            assert token.id == "1"
-
-
-def test_token_text(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
             assert isinstance(token.text, str)
-            token.text = "کورونا"
-            assert token.text == "کورونا"
-
-
-def test_token_misc(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
             assert isinstance(token.misc, str)
-            token.misc = "NER"
-            assert token.misc == "NER"
-
-
-def test_token_words(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
             assert isinstance(token.words, list)
-
-
-def test_token_start_char(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
-            assert token.start_char is None
-
-
-def test_token_end_char(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
-            assert token.end_char is None
-
-
-def test_token_ner(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
-            assert token.ner is None
-            token.ner = "Other"
-            assert token.ner == "Other"
-
-
-def test_token_to_dict(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for token in sentence.tokens:
             assert isinstance(token.to_dict(), list)
+            assert token.start_char is None
+            assert token.end_char is None
+            assert token.ner is None
 
 
-def test_word_init_from_misc(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert word.init_from_misc() is None
-
-
-def test_word_id(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
+def test_word():
+    for sentence in CoNLL.iter_string(CONLL_SENTENCE):
+        sent_meta, words = sentence
+        for word in words:
+            word = Word(word)
+            assert isinstance(word, Word)
             assert isinstance(word.id, str)
-            word.id = "1"
-            assert word.id == "1"
-
-
-def test_word_text(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert isinstance(word.text, str)
-            word.text = "کورونا"
-            assert word.text == "کورونا"
-
-
-def test_word_lemma(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
             assert isinstance(word.lemma, str)
-            word.lemma = "_"
-            assert word.lemma is None
-
-
-def test_word_upos(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
             assert isinstance(word.upos, str)
-            word.upos = "_"
-            assert word.upos is None
-
-
-def test_word_xpos(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
             assert isinstance(word.xpos, str)
-            word.xpos = "_"
-            assert word.xpos is None
-
-
-def test_word_feats(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            if word.pos != 'PUNCT':
-                assert isinstance(word.feats, str)
-
-
-def test_word_head(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert isinstance(word.head, int)
-            word.head = "_"
-            assert word.head is None
-
-
-def test_word_deprel(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert isinstance(word.deprel, str)
-            word.deprel = "_"
-            assert word.deprel is None
-
-
-def test_word_deps(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
             assert word.deps is None
-
-
-def test_word_misc(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert isinstance(word.misc, str)
-            word.misc = "_"
-            assert word.misc is None
-
-
-def test_word_parent(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            assert isinstance(word.parent, Token)
-            word.parent = [{"id": "1"}]
-            assert word.parent == [{"id": "1"}]
-
-
-def test_word_pos(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
+            assert isinstance(word.deprel, str)
             assert isinstance(word.pos, str)
-            word.pos = "_"
-            assert word.pos is None
-
-
-def test_word_to_dict(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
+            if word.upos != "PUNCT":
+                assert isinstance(word.feats, str)
+            assert isinstance(word.head, int)
+            assert word.parent is None
             assert isinstance(word.to_dict(), dict)
-
-
-def test_word_to_conll(tmpdir):
-    """Test Case"""
-    texts = CONLL_SENTENCE.splitlines()
-    file_name = tmpdir.join("test.txt")
-    with open(file_name, "w") as file:
-        for text in texts:
-            text = text.strip()
-            file.write(text + "\n")
-    conll_data = CoNLL.load_file(file_name)
-    doc = Document(conll_data)
-    for sentence in doc.sentences:
-        for word in sentence.words:
             assert isinstance(word.conll(), str)
