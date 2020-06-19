fd78ceeb91642d972c121ae2d6bb2321439c9fdc
Ikram Ali <mrikram1989@gmail.com>
2020-06-19 11:53:46 +0500

    [pytest] test fixed.

--- urduhack/__init__.py
+++ urduhack/__init__.py
@@ -3,8 +3,7 @@
 from .about import __version__, get_info
 from .conll import CoNLL
 from .normalization import normalize
-from .pipeline.core import Pipeline
 from .stop_words import STOP_WORDS
 from .utils.resources import download
 
-__all__ = ["__version__", "get_info", "normalize", "STOP_WORDS", "download", "CoNLL", "Pipeline"]
+__all__ = ["__version__", "get_info", "normalize", "STOP_WORDS", "download", "CoNLL",]
