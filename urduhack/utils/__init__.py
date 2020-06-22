7328e0a01e1072357e3083b304617acf939d8dbd
Ikram Ali <mrikram1989@gmail.com>
2020-06-22 19:16:33 +0500

    [document] bug fixed.

--- urduhack/utils/__init__.py
+++ urduhack/utils/__init__.py
@@ -6,6 +6,5 @@ Utils module
 Collection of helper functions.
 """
 from .io import pickle_load, pickle_dump, download_from_url, remove_file
-from .text import load_vocab
 
-__all__ = ["load_vocab", "pickle_load", "pickle_dump", "download_from_url", "remove_file"]
+__all__ = ["pickle_load", "pickle_dump", "download_from_url", "remove_file"]
