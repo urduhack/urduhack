b9034a6e04b2659a5d40c1f5511533923c3cbc5c
Ikram Ali <mrikram1989@gmail.com>
2020-07-01 15:34:08 +0500

    [preprocess] white spaces added.

--- urduhack/models/pos_tagger/__init__.py
+++ urduhack/models/pos_tagger/__init__.py
@@ -1,5 +1,4 @@
+# coding: utf8
 """POS Tagger Module"""
 
 from urduhack.models.pos_tagger.predict import predict_tags
-
-__all__ = ["predict_tags"]
--- urduhack/models/pos_tagger/predict.py
+++ urduhack/models/pos_tagger/predict.py
@@ -1,19 +1,19 @@
+# coding: utf8
 """POS Tag Prediction Pipeline"""
 
-from urduhack.models.pos_tagger.model import bi_lstm_crf_model
-from urduhack.tokenization import word_tokenizer
-from urduhack.config import POS_TAGGER_WEIGHTS_PATH, POS_WORD2IDX_PATH, POS_TAG2IDX_PATH
-
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
 from typing import Tuple
 
-import tensorflow as tf
 import numpy as np
-import json
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from urduhack.config import POS_TAGGER_WEIGHTS_PATH, POS_WORD2IDX_PATH, POS_TAG2IDX_PATH
+from urduhack.models.pos_tagger.model import _bi_lstm_crf_model
 
 
-def load_metadata(model_path: str, word2idx_path: str,
-                  tag2idx_path: str, max_len: int = 50) -> Tuple[tf.keras.Model, dict, dict, dict, dict]:
+def _load_metadata(model_path: str, word2idx_path: str,
+                   tag2idx_path: str, max_len: int = 50) -> Tuple[tf.keras.Model, dict, dict, dict, dict]:
     """
     Loads model weights and it's metadata
 
@@ -26,7 +26,7 @@ def load_metadata(model_path: str, word2idx_path: str,
     Returns:
         Model and related dictionaries
     """
-    with open(word2idx_path, "r") as w2i, open(tag2idx_path, "r") as t2i:
+    with open(word2idx_path, "r", encoding="utf8") as w2i, open(tag2idx_path, "r", encoding="utf8") as t2i:
         w2i = json.load(w2i)
         t2i = json.load(t2i)
 
@@ -35,30 +35,29 @@ def load_metadata(model_path: str, word2idx_path: str,
     i2w = {i: w for w, i in w2i.items()}
     i2t = {i: w for w, i in t2i.items()}
 
-    model_arch = bi_lstm_crf_model(n_words, n_tags, max_len)
+    model_arch = _bi_lstm_crf_model(n_words, n_tags, max_len)
     model_arch.load_weights(model_path)
 
     return model_arch, w2i, t2i, i2w, i2t
 
 
-model, word2idx, tag2idx, idx2word, idx2tag = load_metadata(POS_TAGGER_WEIGHTS_PATH,
-                                                            POS_WORD2IDX_PATH, POS_TAG2IDX_PATH)
+model, word2idx, tag2idx, idx2word, idx2tag = _load_metadata(POS_TAGGER_WEIGHTS_PATH, POS_WORD2IDX_PATH,
+                                                             POS_TAG2IDX_PATH)
 
 
-def predict_tags(tokens: str, max_len: int = 50) -> list:
+def predict_tags(text: str) -> list:
     """
     Predicts POS Tags
 
     Args:
-        tokens (str): Input text string
-        max_len (int): Maximum sequence length
+        text (str): Input text string
 
     Returns:
-        List containing words their tags
+        list: Containing words their tags
     """
-    tokens = tokens.split()
+    tokens = text.split()
     encoded = [[word2idx[word] if word in word2idx else word2idx["UNK"] for word in tokens]]
-    padded = pad_sequences(sequences=encoded, maxlen=max_len, value=word2idx['PAD'], padding='post')
+    padded = pad_sequences(sequences=encoded, maxlen=50, value=word2idx['PAD'], padding='post')
     predictions = model.predict(padded)
     pred_tags = np.argmax(predictions, axis=2).reshape(predictions.shape[1])
     tags = []
